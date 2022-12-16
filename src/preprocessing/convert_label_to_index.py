4f8f920bd21810ba7d03f8f5fb48039406d54c05
JminJ <jminju254@gmail.com>
2022-12-17 00:20:09 +0900

    commit convert_label_to_index.py

--- src/preprocessing/convert_label_to_index.py
+++ src/preprocessing/convert_label_to_index.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from itertools import product
 
 def convert_label_to_tensor(target_dataframe:pd.DataFrame):
     category_label_dict = {"사실형" : 0, "추론형" : 1, "대화형" : 2, "예측형" : 3}
@@ -7,7 +8,10 @@ def convert_label_to_tensor(target_dataframe:pd.DataFrame):
     tense_label_dict = {"과거" : 0, "현재" : 1, "미래" : 2}
     certainty_label_dict = {"확실" : 0, "불확실" : 1}
 
-    full_label_unique = list(np.unique(target_dataframe.loc[:, "label"]))
+
+    all_label_key_lists = [["사실형", "추론형", "대화형", "예측형"], ["긍정", "부정", "미정"], ["과거", "현재", "미래"], ["확실", "불확실"]]
+    full_label_unique = list(product(*all_label_key_lists))
+    full_label_unique = ["-".join(list(t)) for t in full_label_unique]
 
     for i in range(len(target_dataframe)):
         temp_category = target_dataframe.loc[i, "유형"]
@@ -28,9 +32,9 @@ def convert_label_to_tensor(target_dataframe:pd.DataFrame):
 
 
 if __name__ == "__main__":
-    TARGET_DATA_PATH = "Dacon_JminJ_Sentence_Category_Classification/dataset/train.csv"
+    TARGET_DATA_PATH = "dataset/train.csv"
     dataset = pd.read_csv(TARGET_DATA_PATH)
 
     converted_dataset = convert_label_to_tensor(dataset)
-    converted_dataset.to_csv("Dacon_JminJ_Sentence_Category_Classification/dataset/preprocessed_dataset/convert_label_ver_train.csv", index=False)
+    converted_dataset.to_csv("/Users/jeongminju/Documents/GITHUB/Dacon_JminJ_Sentence_Category_Classification/dataset/preprocessed_dataset/convert_label_ver_train.csv", index=False)
 
