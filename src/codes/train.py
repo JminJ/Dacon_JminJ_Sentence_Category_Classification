fe35beb75a16bff2987834df8522464dcf51f196
JminJ <jminju254@gmail.com>
2022-12-18 23:11:10 +0900

    update train.py

--- src/codes/train.py
+++ src/codes/train.py
@@ -45,7 +45,7 @@ class Trainer:
         self.operation_cls.classifier.to(device)
         self.optimizer = torch.optim.RAdam(self.operation_cls.classifier.parameters(), lr = args.learning_rate, weight_decay=0.15) # weight_decay는 고정 값으로 사용한다
         
-        if self.args.use_lr_warup > 0:
+        if self.args.warmup_rate > 0:
             num_total_steps = len(self.train_dataloader) * args.epochs
             num_warmup_steps = self._calc_num_warmup_step(warmup_rate=args.warmup_rate, num_total_steps=num_total_steps)
             self.learning_rate_schedular = get_cosine_schedule_with_warmup(optimizer=self.optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=num_total_steps)
@@ -172,7 +172,7 @@ class Trainer:
             self.optimizer.zero_grad()
             temp_step_loss.backward()
             self.optimizer.step()
-            if self.args.use_lr_warup > 0:
+            if self.args.warmup_rate > 0:
                 self.learning_rate_schedular.step()
 
     def _train_with_accumulation(self):
@@ -210,7 +210,7 @@ class Trainer:
                 self.optimizer.zero_grad()
                 self.gradscaler.step(self.optimizer)
                 self.gradscaler.update()
-                if self.args.use_lr_warup > 0:
+                if self.args.warmup_rate > 0:
                     self.learning_rate_schedular.step()
     '''
         Return
